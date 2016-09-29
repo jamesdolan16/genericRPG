fb95a7bb6998da561d77761060e13220eaeaf945
MorganBaglin-Clarke <m.baglinclarke@gmail.com>
2016-09-29 18:51:54 +0100

    Improved event reader - fixed traceback

--- genericRpg/source/libraries/eventReader.py
+++ genericRpg/source/libraries/eventReader.py
@@ -1,19 +1,22 @@
+from libraries import fileHandler, lispInterpreter
+
 class EventReader:
     def __init__(self):
-        pass
+        self.fh = fileHandler.FileHandler()
+        self.li = lispInterpreter.LispInterpreter()
     def getEvent(self, eventId):
-        eventList = fileH.readJson("files/events.json")
+        eventList = self.fh.readJson("files/events.json")
         for event in eventList:
             if event["id"] == eventId:
                 return event
     def invokeEvent(self, eventId):
-        eventIn = getEvent(eventId)
+        eventIn = self.getEvent(eventId)
         if eventIn["flag"] == 0 or eventIn["reinvokable"] == 1:
-            eventList = fileH.readJson("files/events.json")
+            eventList = self.fh.readJson("files/events.json")
             for event in eventList:
                 if event["id"] == eventId:
                     event["flag"] = 1
-            fileH.writeJson("files/events.json", eventList)
-            script = getEvent(eventId)["script"]
-            inputP.runScript(script)
+            self.fh.writeJson("files/events.json", eventList)
+            script = self.getEvent(eventId)["script"]
+            self.li.runScript(script)
         
