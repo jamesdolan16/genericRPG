0d8c7abc835731eae3815811b7c350d46bfacf6a
MorganBaglin-Clarke <m.baglinclarke@gmail.com>
2016-09-29 18:03:46 +0100

    Improved event reader - added reinvokable

--- genericRpg/source/libraries/eventReader.py
+++ genericRpg/source/libraries/eventReader.py
@@ -7,5 +7,13 @@ class EventReader:
             if event["id"] == eventId:
                 return event
     def invokeEvent(self, eventId):
-        script = getEvent(eventId)["script"]
-        inputP.runScript(script)
+        eventIn = getEvent(eventId)
+        if eventIn["flag"] == 0 or eventIn["reinvokable"] == 1:
+            eventList = fileH.readJson("files/events.json")
+            for event in eventList:
+                if event["id"] == eventId:
+                    event["flag"] = 1
+            fileH.writeJson("files/events.json", eventList)
+            script = getEvent(eventId)["script"]
+            inputP.runScript(script)
+        
